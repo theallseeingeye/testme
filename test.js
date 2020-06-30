26c820cf717fd9104acc6e4747181b3e272808ca
Jay <hello@jays.email>
2020-06-30 10:14:30 -0600

    added test.js

---
+++ test.js
@@ -0,0 +1,25 @@
+function hellome() {
+    return 'pooop'
+};
+
+
+function secondtest() {
+    return 'test me function'
+}
+
+console.log(secondtest);
+console.log(secondtest);
+console.log(secondtest);
+console.log(secondtest);
+console.log(secondtest);
+console.log(secondtest);
+console.log(secondtest);
+console.log(secondtest);
+console.log(secondtest);
+
+module.exports = {
+    hellome,
+    secondtest
+};
+
+
