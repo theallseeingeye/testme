d0201ed4bc5fdf110543ffdc713dd9bd1bfe6b39
Jay <hello@jays.email>
2020-06-30 09:52:48 -0600

    more logs!

--- index.js
+++ index.js
@@ -16,4 +16,10 @@ testme('Cool Cakes!');
 console.log('hellometest', hellome());
 console.log('seccondtest', secondtest());
 console.log('This is an added feature');
+console.log('This is an added feature');
+console.log('This is an added feature');
+console.log('This is an added feature');
+console.log('This is an added feature');
+console.log('This is an added feature');
+console.log('This is an added feature');
 app.listen(port, () => console.log(`Listening on http://127.0.0.1:${port}`))
